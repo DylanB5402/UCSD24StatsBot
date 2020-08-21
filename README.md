f12b7dad9bcbc59ab935b422a5926ccedda987c5
Dylan <d.barva5402@gmail.com>
2020-08-21 14:26:29 -0700

    get connected to tucsd '24 server

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 UCSD24StatsBot
+
+A discord bot made to gather data about the user-base of the UCSD Class of 2024 Discord Server.
--- src/bot.py
+++ src/bot.py
@@ -1,4 +1,5 @@
 import discord
+import tokens
 
 
 class MyClient(discord.Client):
@@ -6,13 +7,14 @@ class MyClient(discord.Client):
     def __init__(self):
         super().__init__()
         self.task = self.loop.create_task(self.view_stats())
+        self.server_members = []
 
     async def view_stats(self):
         await self.wait_until_ready()
         # if not self.is_closed():
         # print(self.guilds[0].id)
-        bob : discord.member = self.get_guild(688557666779529271).members[0]
-        print(bob.roles)
+        self.server_members = self.get_guild(688557666779529271).members
+        print(self.server_members)
 
 client = MyClient()
-client.run()
+client.run(tokens.token)
